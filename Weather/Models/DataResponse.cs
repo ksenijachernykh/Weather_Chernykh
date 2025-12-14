d149ab82fe0d5b704faed07ec388570887d690ce
Kseniya <ksenijachernyh@yandex.ru>
2025-12-14 19:10:57 +0500

    Реализован класс DataResponse.Черных

---
+++ Weather/Models/DataResponse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices.ActiveDirectory;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weather.Models
+{
+    public class DataResponse
+    {
+        public List<Forecast> forecasts { get; set; }
+    }
+
+    public class Forecast
+    {
+        public DateTime date { get; set; }
+        public List<Hour> hours { get; set; }
+    }
+
+    public class Hour
+    {
+        public string hour { get; set; }
+        public string condition { get; set; }
+        public int humidity { get; set; }
+        public int prec_type { get; set; }
+        public int temp { get; set; }  
+    }
+}
